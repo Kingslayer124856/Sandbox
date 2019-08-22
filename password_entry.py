04e6db6c1c1bdae1ca2699819107b699a475e290
Cassandra King <cassandra.king4@my.jcu.edu.au>
2019-08-22 11:44:12 +1000

    Add Password check program

--- password_entry.py
+++ password_entry.py
@@ -1,1 +1,8 @@
 """Cassandra King"""
+
+password = input("Password: ")
+while len(password) <= 3:
+    print("Password should be longer then 3 characters.")
+    password = input("Password: ")
+else:
+    print('*' * len(password))
